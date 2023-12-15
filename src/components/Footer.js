ec79d4987203658d2f87b2420c21e38b73ad0c45
amarkantsem <amarkant.semwal@newvision-software.com>
2023-12-15 19:52:32 +0530

    msg related to changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,7 @@ function Footer(props) {
         All rights reserved.
         <div className="float-right d-none d-sm-inline-block">
           <b>Version</b> 3.2.0
+          
         </div>
       </footer>
      {/* Control Sidebar */}
